b563505fdb7f9574861e133b9061db604f872936
AconCavy <aconcavy@gmail.com>
2021-11-09 03:11:24 +0900

    Add release.ps1

---
+++ release.ps1
@@ -0,0 +1,20 @@
+function Publish-Release {
+  param (
+    [Parameter(Mandatory = $true)] [string] $Tag,
+    [PSDefaultValue(Help = "./publish")] [string] $Path = "./publish"
+  )
+
+  $PreRelease = $Tag -like 'v*-*-*' ? '-p' : ""
+  $Version = $Tag.Substring(1)
+
+  dotnet pack ./CompetitiveProgrammingTemplate.csproj --configuration Release --output $Path -p:PackageVersion=$Version
+  
+  $Assets = @()
+  Get-ChildItem -Path $Path |
+  ForEach-Object {
+    $Target = $_.Name
+    $Assets += "./$Target"
+  }
+
+  gh release create $Tag $Assets $PreRelease
+}
